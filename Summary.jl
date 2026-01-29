99a9e12880fd1f46cabc49e59f65e52be0f14ea0
Julien Jacques <88965496+Limonade4@users.noreply.github.com>
2026-01-30 00:26:24 +0100

    Add files via upload

---
+++ Summary.jl
@@ -0,0 +1,61 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 28749dd2-fd69-11f0-a5cf-69233d813aa0
+begin
+    # On utilise la fonction HTML de Pluto pour faire de jolis liens
+    html"""
+    <div style="
+        background-color: #f0f8ff; 
+        padding: 20px; 
+        border-radius: 10px; 
+        border: 2px solid #87cefa;">
+        
+        <h1 style="color: #2c3e50;">🧬 Cours de Biologie Numérique</h1>
+        <p style="color: black;">Bienvenue ! Choisissez un module ci-dessous pour lancer la simulation interactive.</p>
+        
+        <ul>
+            <li style="margin: 10px 0;">
+                <a href="./open?path=Simulation_derive_genetique.jl" 
+                   style="font-size: 18px; font-weight: bold; color: #007bff; text-decoration: none;">
+                   👉 Module 1 : La Dérive Génétique (Simulation)
+                </a>
+            </li>
+            
+            <li style="margin: 10px 0;">
+                <a href="./open?path=selection_naturelle.jl" 
+                   style="font-size: 18px; font-weight: bold; color: #007bff; text-decoration: none;">
+                   👉 Module 2 : La Sélection Naturelle (À venir)
+                </a>
+            </li>
+            
+        </ul>
+        
+        <p><i>Note : Le chargement d'un module peut prendre quelques secondes.</i></p>
+    </div>
+    """
+end
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═28749dd2-fd69-11f0-a5cf-69233d813aa0
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
